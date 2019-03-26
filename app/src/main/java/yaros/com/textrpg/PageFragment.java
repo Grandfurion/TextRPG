f4809ee5bda8c2dd53ae0500a89b9bf53d0aa58f
Grandfurion <jarikpar@gmail.com>
2019-03-26 15:40:05 +0200

    update

--- app/src/main/java/yaros/com/textrpg/PageFragment.java
+++ app/src/main/java/yaros/com/textrpg/PageFragment.java
@@ -7,6 +7,8 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public class PageFragment extends Fragment {
@@ -19,11 +21,16 @@ public class PageFragment extends Fragment {
         return new PageFragment();
     }
 
-    public static void Update(int new_id, TextView pageText){
+    public void Update(int new_id, TextView pageText, LinearLayout linearLayout){
         id = new_id;
 
         pageText.setText(MainActivity.chapters.get(id).CHAPTER_TEXT);
+        int i = 0;
         for (int variant :MainActivity.chapters.get(id).CHAPTER_VARIANTIDS) {
+            Button button = new Button(this.getContext());
+            button.setText((MainActivity.chapters.get(id).CHAPTER_VARIANTIDS[i]));
+            linearLayout.addView(button);
+            i++;
             //СОЗДАЕМ КНОПКИ ДИНАМИЧНО (ИЛИ НЕ ОЧЕНЬ)
         }
     }
@@ -38,8 +45,9 @@ public class PageFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
 
         final TextView pageText = getView().findViewById(R.id.pageText);
+        final LinearLayout linearLayout = getView().findViewById(R.id.buttonsLayout);
 
-        Update(id, pageText);
+        Update(id, pageText, linearLayout);
         //MainActivity.chapters.get(id);
 
         super.onViewCreated(view, savedInstanceState);
