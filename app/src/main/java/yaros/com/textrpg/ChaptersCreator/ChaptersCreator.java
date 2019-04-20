a2087cf58e1501724eb078e1fcf288b856df768f
Grandfurion <jarikpar@gmail.com>
2019-04-20 18:28:12 +0300

    RABOTAET!!

--- app/src/main/java/yaros/com/textrpg/ChaptersCreator/ChaptersCreator.java
+++ app/src/main/java/yaros/com/textrpg/ChaptersCreator/ChaptersCreator.java
@@ -24,18 +24,18 @@ public class ChaptersCreator {
         HashMap<Integer,Chapter> chapters = new HashMap<>(initCap);
 
         TravelToNode travelToNode = new TravelToNode();
-        for(int i = 0; i < initCap; i++){
+        for(int i = 0; i<initCap; i++){
 
             String chapter = "chapter_" + i;
 
-            int chapId = context.getResources().getIdentifier(chapter,"string", context.getPackageName());
+            int chapId = context.getResources().getIdentifier(("chapter_" + i),"string", context.getPackageName());
 
             //int[] variants = GetVariants.getVariants(context.getString(chapId));
             int[] variants = GetVariants.getVariants(chapId, context);
             /*int[] trueVariants = new int[variants.length];
             for (int a = 0; a < variants.length; a++) {
                 trueVariants[a] = context.getResources().getIdentifier((chapter), "")
-            }/*
+            }
             /*chapters.put(i, new Chapter(i, context.getResources().
                     getIdentifier("chapter_" + i, "string", context.getPackageName()), variants));*/
             chapters.put(i, new Chapter(i, chapId, variants));
